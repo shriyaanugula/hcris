6e1714508c73ae0853250045a03d0588599b8644
Ian McCarthy <ianmccarthy.econ@gmail.com>
2019-05-31 14:46:39 -0400

    Collapse hospital/report/year into hospital/year obsevations, with year defined by fiscal year listed in report

--- data-code/_HCRIS_Data.R
+++ data-code/_HCRIS_Data.R
@@ -27,13 +27,15 @@ final.hcris.v1996 = final.hcris.v1996 %>%
 
 ## combine v1996 and v2010 hcris forms, and sort by provider_number/year
 final.hcris=rbind(final.hcris.v1996,final.hcris.v2010) %>%
-  arrange(provider_number,year) %>%
-  mutate(fy_end=mdy(fy_end),fy_start=mdy(fy_start))
-final.hcris
+  mutate(fy_end=mdy(fy_end),fy_start=mdy(fy_start),
+         date_processed=mdy(date_processed),date_created=mdy(date_created),
+         tot_discounts=abs(tot_discounts), hrrp_payment=abs(hrrp_payment)) %>%
+  mutate(fyear=year(fy_end)) %>%
+  arrange(provider_number,fyear) %>%
+  select(-year)
 
 ## count of hospitals/provider_number by year
-final.hcris %>% group_by(year) %>% count()
-
+final.hcris %>% group_by(fyear) %>% count()
 
 ########################################################################################
 ## Spot check with original data pull from SQL/Access
@@ -53,54 +55,112 @@ final.hcris.v2010 %>% filter(provider_number==332008, year==2015) %>% select(anc
 HCRIS.check %>% filter(Provider_Number==332008) %>% select(AncServ_Charges,Total_Discharges,Mcare_Discharges,Mcaid_Discharges,Tot_McarePymt,Sec_McarePymt,HVBP_Payment,HRRP_Payment)
 
 
-
 ########################################################################################
 ## Clean data for final analytic file
 ########################################################################################
-## unique report/hospital/year indicators
-final.hcris =
-  final.hcris %>% 
-  add_count(provider_number, year, name="total_reports")
 
+## create count of reports by hospital fiscal year
 final.hcris =
   final.hcris %>% 
-  group_by(provider_number, year) %>%
+  add_count(provider_number, fyear, name="total_reports")
+
+## create running total of reports
+final.hcris =
+  final.hcris %>% 
+  group_by(provider_number, fyear) %>%
   mutate(report_number=row_number())
 
+## identify hospitals with only one report per fiscal year 
+## this will be the first set of hospitals in the final dataset
 unique.hcris1 =
   final.hcris %>%
   filter(total_reports==1) %>%
-  select(-total_reports, -report_number)
-  
+  select(-report, -total_reports, -report_number, -npi, -status) %>%
+  mutate(source='unique reports')
+
+
+## identify hospitals with multiple reports per fiscal year
 duplicate.hcris = 
   final.hcris %>%
   filter(total_reports>1) %>%
-  select(-total_reports, -report_number) %>%
-  mutate(fy_end=as_date
-         
-         time_diff=yday(fy_end)-yday(fy_start))
+  mutate(time_diff=fy_end-fy_start)
 
+## calculate elapsed time between fy start and fy end for hospitals with multiple reports
+duplicate.hcris = 
+  duplicate.hcris %>% 
+  group_by(provider_number, fyear) %>%
+  mutate(total_days=sum(time_diff))
 
-
-duplicate.hcris %>% 
-  select(report, provider_number, year, fy_start, fy_end, date_processed, status, tot_charges) %>%
-  arrange(provider_number, year, report)
-
-unique.hcris1 %>% filter(provider_number=='010019') %>% select(fy_start, fy_end)
-duplicate.hcris %>% filter(provider_number=='010019') %>% select(fy_start, fy_end)
-
-clean.duplicates = 
-  duplicate.hcris %>%
-  arrange(provider_number, year, date_processed) %>%
-  group_by(provider_number, year) %>%
-  mutate(max_charge=max(tot_charges))
-
-clean.duplicates %>% 
-  select(report, provider_number, year, fy_start, fy_end, date_processed, tot_charges, max_charge) %>%
-  arrange(provider_number, year, report, fy_start, desc(fy_end), desc(date_processed))
-
+## if the elapsed time within a fy sums to around 365, then just take the total of the two
+## this will be the second set of hospitals in the final dataset
 unique.hcris2 = 
-  clean.duplicates
+  duplicate.hcris %>%
+  filter(total_days<370) %>%
+  group_by(provider_number, fyear) %>%
+  mutate(hrrp_payment=if_else(is.na(hrrp_payment),0,hrrp_payment),
+         hvbp_payment=if_else(is.na(hvbp_payment),0,hvbp_payment)) %>%
+  summarize(beds=max(beds), tot_charges=sum(tot_charges), tot_discounts=sum(tot_discounts),
+            tot_operating_exp=sum(tot_operating_exp), ip_charges=sum(ip_charges),
+            icu_charges=sum(icu_charges), ancillary_charges=sum(ancillary_charges),
+            tot_discharges=sum(tot_discharges), mcare_discharges=sum(mcare_discharges),
+            mcaid_discharges=sum(mcaid_discharges), tot_mcare_payment=sum(tot_mcare_payment),
+            secondary_mcare_payment=sum(secondary_mcare_payment), hvbp_payment=sum(hvbp_payment),
+            hrrp_payment=sum(hrrp_payment), fy_start=min(fy_start), fy_end=max(fy_end),
+            date_processed=max(date_processed), date_created=min(date_created), 
+            street=first(street), city=first(city), state=first(state),
+            zip=first(zip), county=first(county)) %>%
+  mutate(source='total for year')
 
+## identify hospitals with more than one report and with elapsed time exceeding 370 days
+duplicate.hcris2 =
+  duplicate.hcris %>%
+  filter(total_days>=370) %>%
+  mutate(max_days=max(time_diff), max_date=max(fy_end))
 
-duplicate.hcris
+## identify hospitals with one report (out of multiple) that appears to cover the full year
+## this will be the third set of hospitals in the final dataset
+unique.hcris3 = 
+  duplicate.hcris2 %>%
+  filter(max_days==time_diff, time_diff>360, max_date==fy_end) %>%
+  select(-report, -total_reports, -report_number, -npi, -status, -max_days, -time_diff, -total_days, -max_date) %>%
+  mutate(source='primary report')
+
+## identify remaining hospitals (those with more than one report that cover more than one full year and that do
+##   not appear to have one report that takes up the full year)
+## these hospitals appear to have changed their fiscal years
+duplicate.hcris3=anti_join(duplicate.hcris2, unique.hcris3, by=c("provider_number", "fyear"))
+duplicate.hcris3 =
+  duplicate.hcris3 %>%
+  mutate(total_days=as.integer(total_days), time_diff=as.integer(time_diff)) %>%
+  mutate_at(c("tot_charges","tot_discounts", "tot_operating_exp", "ip_charges",
+              "icu_charges", "ancillary_charges", "tot_discharges", "mcare_discharges",
+              "mcaid_discharges", "tot_mcare_payment", "secondary_mcare_payment",
+              "hvbp_payment", "hrrp_payment"),list(~ .*(time_diff/total_days)))
+
+## form weighted average of values for each fiscal year
+unique.hcris4 = 
+  duplicate.hcris3 %>%
+  group_by(provider_number, fyear) %>%
+  mutate(hrrp_payment=if_else(is.na(hrrp_payment),0,hrrp_payment),
+         hvbp_payment=if_else(is.na(hvbp_payment),0,hvbp_payment)) %>%
+  summarize(beds=max(beds), tot_charges=sum(tot_charges), tot_discounts=sum(tot_discounts),
+            tot_operating_exp=sum(tot_operating_exp), ip_charges=sum(ip_charges),
+            icu_charges=sum(icu_charges), ancillary_charges=sum(ancillary_charges),
+            tot_discharges=sum(tot_discharges), mcare_discharges=sum(mcare_discharges),
+            mcaid_discharges=sum(mcaid_discharges), tot_mcare_payment=sum(tot_mcare_payment),
+            secondary_mcare_payment=sum(secondary_mcare_payment), hvbp_payment=sum(hvbp_payment),
+            hrrp_payment=sum(hrrp_payment), fy_start=min(fy_start), fy_end=max(fy_end),
+            date_processed=max(date_processed), date_created=min(date_created), 
+            street=first(street), city=first(city), state=first(state),
+            zip=first(zip), county=first(county)) %>%
+  mutate(source='weighted_average')
+  
+
+########################################################################################
+## Create and save final datasets
+########################################################################################
+final.hcris.data=rbind(unique.hcris1, unique.hcris2, unique.hcris3, unique.hcris4)
+final.hcris.data =
+  final.hcris.data %>%
+  rename(year=fyear) %>%
+  arrange(provider_number, year)
